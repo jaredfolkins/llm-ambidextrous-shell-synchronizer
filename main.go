bb206c29d7a4eef0017bdefa4e6f25f7ffd51cd1
Jared Folkins <jfolkins@gmail.com>
2025-03-02 17:30:26 -0800

    enhc: favico(s) added

--- main.go
+++ main.go
@@ -647,6 +647,11 @@ func printHTML(w http.ResponseWriter, html string) {
 	fmt.Fprintf(w, `<!DOCTYPE html>
 	<html>
 	<head>
+		<link rel="icon" type="image/png" href="/assets/favicon-96x96.png" sizes="96x96" />
+		<link rel="icon" type="image/svg+xml" href="/assets/favicon.svg" />
+		<link rel="shortcut icon" href="/assets/favicon.ico" />
+		<link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png" />
+		<link rel="manifest" href="/assets/site.webmanifest" />
 		<title>LLMASS - LLM Ambidextrous Shell Synchronizer</title>
 		<link rel="stylesheet" href="/assets/style.css">
 	</head>
